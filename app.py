0f8f8dea2f14f4970871c842c50b6682fd26545e
nelsonvelis <nvelis@terpmail.umd.edu>
2023-03-30 00:05:12 -0400

    learning the flasks fundamentals

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,19 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('Favorite course subject: ' + request.args.get('subject'))
+    print('Favorite course number: ' + request.args.get('course_number'))
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
